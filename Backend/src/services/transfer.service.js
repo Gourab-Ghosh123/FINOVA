de6ee7a29c4e9d218b4c560c1400745f47175b63
Gourab-Ghosh123 <ggourab2004@gmail.com>
2026-08-14 15:48:09 +0530

    seperated Application from controllers

--- Backend/src/services/transfer.service.js
+++ Backend/src/services/transfer.service.js
@@ -1,8 +1,14 @@
 const createTransfer = (transferData) => {
     console.log(transferData);
 
+    const fine = transferData.amount * 0.01;
+    const totalAmount = transferData.amount + fine;
+
     return {
         message : "Proceeded Successfully!",
+        amount : transferData.amount,
+        fine : fine,
+        totalAmount : totalAmount,
         transfer : transferData
     };
 }
