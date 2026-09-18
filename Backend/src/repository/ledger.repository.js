3308ec434e269b4ebe211f2ae5460b7ba475e7c8
Gourab-Ghosh123 <ggourab2004@gmail.com>
2026-09-18 18:30:09 +0530

    Get Balance From Ledger Entries

--- Backend/src/repository/ledger.repository.js
+++ Backend/src/repository/ledger.repository.js
@@ -11,4 +11,44 @@ const createLedgerEntry = async(client , transactionId , accountId , entryType ,
     return result.rows[0];
 }
 
-module.exports = {createLedgerEntry};
+const getLedgerEntryByTransaction = async(client , transactionId) => {
+    const query = `SELECT * FROM ledger_entries
+    WHERE transaction_id = $1
+    ORDER BY id ASC
+    `;
+
+    const result = await client.query(
+        query,
+        [transactionId]
+    );
+    return result.rows;
+}
+
+const getBalanceFromLedger = async(client , accountId) => {
+    const query = `
+        SELECT 
+            COALESCE(
+                SUM(
+                    CASE
+                        WHEN entry_type = "CREDIT"
+                        THEN amount_paise
+
+                        WHEN entry_type = "DEBIT"
+                        THEN -amount_paise
+                    END
+                ),
+                0
+            ) AS balance_paise
+             FROM ledger_entries
+             WHERE account_id = $1
+    `;
+
+    const reuslt = await client.query(
+        query,
+        [accountId]
+    );
+
+    return reuslt.rows[0];
+}
+
+module.exports = {createLedgerEntry , getLedgerEntryByTransaction , getBalanceFromLedger};
